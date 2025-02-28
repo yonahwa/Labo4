811f766d0049d231344ae7834e89207c43d33a40
Jonah Wack <2330034@collegebart.ca>
2025-02-28 11:33:12 -0500

    moves update

--- src/Classe.h
+++ src/Classe.h
@@ -3,10 +3,26 @@
 class Ball
 {
 private:
-    int initialPosX = 150;
-    int initialPosY = 250;
+    int PosX = 150;
+    int PosY = 250;
 public:
-    
+    void Movement(int padPosX){
+        int dirX, dirY = 1;
+        if(PosY < 0 || PosY >500){
+            dirY *= -1;
+        }
+        else {
+            PosY += dirY;
+        }
+
+        if(PosX > padPosX && PosX < padPosX+50){
+            dirX *= -1;
+        }
+        else{
+            PosX += dirX;
+        }
+        DrawCircle(PosX,PosY,5,PURPLE);
+    }
 };
 
 class Paddle
@@ -15,7 +31,16 @@ private:
     int PosX = 150;
     int PosY = 250;
 public:
-    bool isKeydown(int key);
+    void Move (){
+        bool isKeydown(int key);
+        if(isKeydown(KEY_A)){
+            PosX -= 1;
+        }
+        if(isKeydown(KEY_D)){
+            PosX +=1;
+        }
+        DrawRectangle(PosX,PosY,50,10,WHITE);
+    }
     
 };
 
